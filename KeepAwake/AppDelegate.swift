bda0a5d55e9427b1f76956dda990e05b3e01f779
Sean McElholm <seanmcelholm@gmail.com>
2018-10-03 14:33:04 +0100

    Commit latest changes

--- KeepAwake/AppDelegate.swift
+++ KeepAwake/AppDelegate.swift
@@ -7,24 +7,74 @@
 //
 
 import Cocoa
+import ServiceManagement
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-	let task = Process()
 	
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
+	let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+	var toggleAwake: Bool!
+	let task = Process()
+	var toggleString:String = "Enable"
+	let menu = NSMenu()
+	
+	func applicationDidFinishLaunching(_ aNotification: Notification)
+	{
 		// Insert code here to initialize your application
-		task.launchPath = "/usr/bin/caffeinate"
-		task.arguments = ["-d"]
-		task.launch()
+		if let button = statusItem.button {
+			button.image = NSImage(named: NSImage.Name("StatusBarButtonImage"))
+			button.action = #selector(ToggleAwakeStatus(_:))
+		}
+		ConstructMenu()
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
 		// Insert code here to tear down your application
-		task.terminate()
+		if task.isRunning
+		{
+			task.terminate()
+		}
 	}
 
-
+	@objc func ToggleAwakeStatus(_ sender: Any?)
+	{
+		if let button = statusItem.button
+		{
+			if toggleAwake == true
+			{
+				toggleString = "Enable"
+				toggleAwake = false
+				task.terminate()
+				button.image = NSImage(named: NSImage.Name("StatusBarButtonImage"))
+			}
+			else
+			{
+				toggleString = "Disable"
+				toggleAwake = true
+				task.launchPath = "/usr/bin/caffeinate"
+				task.arguments = ["-d"]
+				task.launch()
+				
+				button.image = NSImage(named: NSImage.Name("StatusBarActive"))
+			}
+			ConstructMenu()
+		}
+	}
+	
+	@objc func ToggleLaunchStartup(_ sender: Any?)
+	{
+		SMLoginItemSetEnabled(Bundle.main.infoDictionary![kCFBundleIdentifierKey as String] as! CFString, true)
+	}
+	
+	func ConstructMenu()
+	{
+		menu.removeAllItems()
+		menu.addItem(NSMenuItem(title: toggleString, action: #selector(AppDelegate.ToggleAwakeStatus(_:)), keyEquivalent: "T"))
+		menu.addItem(NSMenuItem.separator())
+		menu.addItem(NSMenuItem(title: "Launch at startup", action: #selector(AppDelegate.ToggleLaunchStartup(_:)), keyEquivalent: "P"))
+		menu.addItem(NSMenuItem.separator())
+		menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+		statusItem.menu = menu
+	}
 }
 
